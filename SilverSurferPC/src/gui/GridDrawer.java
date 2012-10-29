6cb467570004aca0ea5b2d2ac4f89d3f3059f050
Sam Gielis <samgielis@gmail.com>
2012-10-29 15:28:55 +0100

    SimulationPanel heeft nu een fancy grid-behangpapiertje.

---
+++ SilverSurferPC/src/gui/GridDrawer.java
@@ -0,0 +1,26 @@
+package gui;
+
+import simulator.SimulationJPanel;
+import simulator.SimulationPilot;
+
+public class GridDrawer {
+
+	private SimulationJPanel panel;
+	public GridDrawer(SimulationJPanel panel){
+		
+	}
+	
+	public void setPanel(SimulationJPanel panel) {
+		this.panel = panel;
+	}
+	
+	public SimulationJPanel getPanel() {
+		return panel;
+	}
+	
+	public void drawGrid() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
