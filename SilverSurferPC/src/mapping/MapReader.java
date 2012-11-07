ff7a830bebf2cc1c22daa278655bcca749856be6
Toon Nolten <toon.nolten@student.kuleuven.be>
2012-11-07 12:09:22 +0100

    Allerlei warnings opgelost

--- SilverSurferPC/src/mapping/MapReader.java
+++ SilverSurferPC/src/mapping/MapReader.java
@@ -1,13 +1,6 @@
 package mapping;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
