f560cd5cae76f23cbd441f9643a86c6c538c122c
MaximVanMechelen <maximvanmechelen@hotmail.com>
2012-11-12 16:31:06 +0100

    look around and various

--- SilverSurferNXT/src/statemachine/TurnLeft.java
+++ SilverSurferNXT/src/statemachine/TurnLeft.java
@@ -5,11 +5,8 @@ import communication.CommandUnit;
 import lejos.nxt.*;
 
 public class TurnLeft extends State {
-	SongThread playSong = new SongThread();
 	
 	public TurnLeft() {
-		
-		playSong.start();
 		Motor.A.setSpeed(CommandUnit.NORMAL_SPEED);
 		Motor.B.setSpeed(CommandUnit.NORMAL_SPEED);
 		Motor.A.forward();
@@ -18,37 +15,31 @@ public class TurnLeft extends State {
 	
 	@Override
 	public State ForwardPressed() {
-		playSong.flag = false;
 		return new DrivingForwardLeft();
 	}
 	
 	@Override
 	public State ForwardReleased() {
-		playSong.flag = false;
 		return new DrivingBackwardLeft();
 	}
 	
 	@Override
 	public State BackwardPressed() {
-		playSong.flag = false;
 		return new DrivingBackwardLeft();
 	}
 	
 	@Override
 	public State BackwardReleased() {
-		playSong.flag = false;
 		return new DrivingForwardLeft();
 	}
 	
 	@Override
 	public State LeftReleased() {
-		playSong.flag = false;
 		return new Waiting();
 	}
 	
 	@Override
 	public State RightPressed() {
-		playSong.flag = false;
 		return new Waiting();
 	}
 }
