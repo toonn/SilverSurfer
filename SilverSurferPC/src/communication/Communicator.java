c951ec4d7a498b5b4bfbcc3e527ca3282e28977d
Toon Nolten <toon.nolten@student.kuleuven.be>
2012-12-03 16:51:39 +0100

    Sensor grafieken toegevoegd; (error message in communicator aangepast)

--- SilverSurferPC/src/communication/Communicator.java
+++ SilverSurferPC/src/communication/Communicator.java
@@ -10,197 +10,201 @@ import java.io.*;
 import lejos.pc.comm.*;
 
 public class Communicator {
-	private StatusInfoBuffer statusInfoBuffer;
-	private SimulationPilot simulationPilot = new SimulationPilot();
-	private boolean robotConnected = false;
-	private static DataInputStream dis;
-	private static DataOutputStream dos;
-	private static NXTConnector connection;
-	private static String deviceURL = "00:16:53:0A:04:5A";
-	private static String deviceName = "Silver";
-	private static InfoReceiverThread IRT;
-	private boolean buzy = false;
+    private StatusInfoBuffer statusInfoBuffer;
+    private SimulationPilot simulationPilot = new SimulationPilot();
+    private boolean robotConnected = false;
+    private static DataInputStream dis;
+    private static DataOutputStream dos;
+    private static NXTConnector connection;
+    private static String deviceURL = "00:16:53:0A:04:5A";
+    private static String deviceName = "Silver";
+    private static InfoReceiverThread IRT;
+    private boolean buzy = false;
 
-	public Communicator(StatusInfoBuffer statusInfoBuffer) {
-		setStatusInfoBuffer(statusInfoBuffer);
-		setSpeed(2);
-	}
-	
-	public StatusInfoBuffer getStatusInfoBuffer() {
-		return statusInfoBuffer;
-	}
-	
-	public void setStatusInfoBuffer(StatusInfoBuffer statusInfoBuffer) {
-		this.statusInfoBuffer = statusInfoBuffer;
-	}
-	
-	public SimulationPilot getSimulationPilot() {
-		return simulationPilot;
-	}
-	
-	public boolean getRobotConnected() {
-		return robotConnected;
-	}
-	
-	public void setRobotConnected(boolean robotConnected) throws Exception {
-		if(robotConnected)
-			openRobotConnection();
-		else
-			closeRobotConnection();
-		this.robotConnected = robotConnected;
-		setSpeed(2);
-	}
-	
-	public void openRobotConnection() throws Exception {	
-		connection = new NXTConnector();
-		connection.connectTo(deviceName, deviceURL, NXTCommFactory.BLUETOOTH, NXTComm.PACKET);
-		dis = connection.getDataIn();
-		dos = connection.getDataOut();
-		if(dis == null || dos == null)
-			throw new IOException();
-		IRT = new InfoReceiverThread(statusInfoBuffer);
-		IRT.setDis(dis);
-		IRT.setDos(dos);
-		IRT.start();
-	}
-	
-	public void closeRobotConnection() throws Exception {
-		dos.writeInt(Command.CLOSE_CONNECTION);
-		dos.flush();
-		IRT.setQuit(true);
-		dis.close();
-		dos.close();
-		connection.close();
-	}
-	
-	public void setBuzy(boolean buzy) {
-		this.buzy = buzy;
-	}
-	
-	public void sendCommand(int command) {
-		try {
-			if(robotConnected) {
-				dos.writeInt(command);
-				dos.flush();
-				if(command == Command.checkObstructionsAndSetTile) {
-					buzy = true;
-					while(buzy)
-						Thread.sleep(100);
-				}
-			}
-			if(command == Command.SLOW_SPEED)
-				simulationPilot.setSpeed(194);
-			else if(command == Command.NORMAL_SPEED)
-				simulationPilot.setSpeed(86);
-			else if(command == Command.FAST_SPEED)
-				simulationPilot.setSpeed(58);
-			else if(command == Command.VERY_FAST_SPEED)
-				simulationPilot.setSpeed(48);				
-			else if(command == Command.ALIGN_PERPENDICULAR)
-				simulationPilot.allignOnWhiteLine();
-			else if(command == Command.ALIGN_WALL)
-				simulationPilot.allignOnWalls();
-			else if(command == Command.LOOK_AROUND)
-	            simulationPilot.checkForObstructions();		
-			else if(command == Command.PLAY_SONG) {
-	    			SongThread ST = new SongThread(); 
-	    			ST.start();
-			}
-			else if(command == Command.checkObstructionsAndSetTile && !robotConnected){
-				simulationPilot.checkForObstructionAndSetTile();
-			}
-			else if(command%100 == Command.AUTOMATIC_MOVE_FORWARD) {
-				int amount = (command-Command.AUTOMATIC_MOVE_FORWARD)/100;
-				while(amount != 0) {
-					simulationPilot.travel(1);
-					amount = amount - 1;
-				}
-			}
-			else if(command%100 == Command.AUTOMATIC_TURN_ANGLE) {
-				double amount = (double) (command-Command.AUTOMATIC_TURN_ANGLE)/100;
-				while(amount > 0) {
-					simulationPilot.rotate(1);
-					amount = amount - 1;
-				}
-			}
-			else if(command%100 == -(100-Command.AUTOMATIC_TURN_ANGLE)) {
-				double amount = (double) (command-Command.AUTOMATIC_TURN_ANGLE)/100;
-				while(amount < 0) {
-					simulationPilot.rotate(-1);
-					amount = amount + 1;
-				}
-			}
-		} catch(Exception e) {
-			System.out.println("Error in Communicator.sendCommand(int command)!");
-		}
-	}
+    public Communicator(StatusInfoBuffer statusInfoBuffer) {
+        setStatusInfoBuffer(statusInfoBuffer);
+        setSpeed(2);
+    }
 
-	public void moveTurn(int lengthInCM, int anglesInDegrees, int amtOfAngles) {
-		int length = lengthInCM*100 + Command.AUTOMATIC_MOVE_FORWARD;
-		int angles;
-		if(amtOfAngles == 0) {
-			angles = anglesInDegrees*100 + Command.AUTOMATIC_TURN_ANGLE;
-			sendCommand(length);
-			sendCommand(angles);
-		}
-		else {
-			angles = (int)Math.round(360.0/amtOfAngles)*100 + Command.AUTOMATIC_TURN_ANGLE;
-			if (amtOfAngles == 1)
-				sendCommand(length);
-			else for(int i = 0; i<amtOfAngles; i++) {
-				sendCommand(length);
-				sendCommand(angles);
-			}			
-		}
-	}
-	
-	public int getSpeed() {
-		return simulationPilot.getSpeed();
-	}
-	
-	public void setSpeed(int speed) {
-		if(speed == 1)
-			sendCommand(Command.SLOW_SPEED);
-		else if(speed == 2)
-			sendCommand(Command.NORMAL_SPEED);
-		else if(speed == 3)
-			sendCommand(Command.FAST_SPEED);
-		else
-			sendCommand(Command.VERY_FAST_SPEED);
-	}
+    public StatusInfoBuffer getStatusInfoBuffer() {
+        return statusInfoBuffer;
+    }
 
-	public String getConsoleTag() {
-		if(robotConnected)
-			return "[ROBOT]";
-		return "[SIMULATOR]";
-	}
-	
-	public void clear() {
-		simulationPilot.clear();
-	}
-	
-	
-	//TODO: vanaf hier!
-	/**
-	 * Gets the amount of angles the arrow should turn in one event
-	 * to be at par with the robot.
-	 */
-	public double getAngularSpeed() {
-		switch(getSpeed()) {
-		case 1: return (double) 1.82;
-		case 2: return (double) 2.74;
-		case 3: return (double) 2.77;
-		case 4: return (double) 1.82;
-		}
-		return (double) 2.74;
-	}
-	
-	public void goToNextTile(Orientation orientation) throws IOException{
-		double currentAngle = getStatusInfoBuffer().getAngle();
-		int angleToRotate = (int)((double) orientation.getRightAngle() - currentAngle);
-		angleToRotate = (int) ExtMath.getSmallestAngle(angleToRotate);
-		sendCommand(angleToRotate*100 + Command.AUTOMATIC_TURN_ANGLE);
-		sendCommand(40*100 + Command.AUTOMATIC_MOVE_FORWARD);
-	}
+    public void setStatusInfoBuffer(StatusInfoBuffer statusInfoBuffer) {
+        this.statusInfoBuffer = statusInfoBuffer;
+    }
+
+    public SimulationPilot getSimulationPilot() {
+        return simulationPilot;
+    }
+
+    public boolean getRobotConnected() {
+        return robotConnected;
+    }
+
+    public void setRobotConnected(boolean robotConnected) throws Exception {
+        if (robotConnected)
+            openRobotConnection();
+        else
+            closeRobotConnection();
+        this.robotConnected = robotConnected;
+        setSpeed(2);
+    }
+
+    public void openRobotConnection() throws Exception {
+        connection = new NXTConnector();
+        connection.connectTo(deviceName, deviceURL, NXTCommFactory.BLUETOOTH,
+                NXTComm.PACKET);
+        dis = connection.getDataIn();
+        dos = connection.getDataOut();
+        if (dis == null || dos == null)
+            throw new IOException();
+        IRT = new InfoReceiverThread(statusInfoBuffer);
+        IRT.setDis(dis);
+        IRT.setDos(dos);
+        IRT.start();
+    }
+
+    public void closeRobotConnection() throws Exception {
+        dos.writeInt(Command.CLOSE_CONNECTION);
+        dos.flush();
+        IRT.setQuit(true);
+        dis.close();
+        dos.close();
+        connection.close();
+    }
+
+    public void setBuzy(boolean buzy) {
+        this.buzy = buzy;
+    }
+
+    public void sendCommand(int command) {
+        try {
+            if (robotConnected) {
+                dos.writeInt(command);
+                dos.flush();
+                if (command == Command.checkObstructionsAndSetTile) {
+                    buzy = true;
+                    while (buzy)
+                        Thread.sleep(100);
+                }
+            }
+            if (command == Command.SLOW_SPEED)
+                simulationPilot.setSpeed(194);
+            else if (command == Command.NORMAL_SPEED)
+                simulationPilot.setSpeed(86);
+            else if (command == Command.FAST_SPEED)
+                simulationPilot.setSpeed(58);
+            else if (command == Command.VERY_FAST_SPEED)
+                simulationPilot.setSpeed(48);
+            else if (command == Command.ALIGN_PERPENDICULAR)
+                simulationPilot.allignOnWhiteLine();
+            else if (command == Command.ALIGN_WALL)
+                simulationPilot.allignOnWalls();
+            else if (command == Command.LOOK_AROUND)
+                simulationPilot.checkForObstructions();
+            else if (command == Command.PLAY_SONG) {
+                SongThread ST = new SongThread();
+                ST.start();
+            } else if (command == Command.checkObstructionsAndSetTile
+                    && !robotConnected) {
+                simulationPilot.checkForObstructionAndSetTile();
+            } else if (command % 100 == Command.AUTOMATIC_MOVE_FORWARD) {
+                int amount = (command - Command.AUTOMATIC_MOVE_FORWARD) / 100;
+                while (amount != 0) {
+                    simulationPilot.travel(1);
+                    amount = amount - 1;
+                }
+            } else if (command % 100 == Command.AUTOMATIC_TURN_ANGLE) {
+                double amount = (double) (command - Command.AUTOMATIC_TURN_ANGLE) / 100;
+                while (amount > 0) {
+                    simulationPilot.rotate(1);
+                    amount = amount - 1;
+                }
+            } else if (command % 100 == -(100 - Command.AUTOMATIC_TURN_ANGLE)) {
+                double amount = (double) (command - Command.AUTOMATIC_TURN_ANGLE) / 100;
+                while (amount < 0) {
+                    simulationPilot.rotate(-1);
+                    amount = amount + 1;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error in Communicator.sendCommand(" + command
+                    % 100 + ")!");
+            e.printStackTrace();
+        }
+    }
+
+    public void moveTurn(int lengthInCM, int anglesInDegrees, int amtOfAngles) {
+        int length = lengthInCM * 100 + Command.AUTOMATIC_MOVE_FORWARD;
+        int angles;
+        if (amtOfAngles == 0) {
+            angles = anglesInDegrees * 100 + Command.AUTOMATIC_TURN_ANGLE;
+            sendCommand(length);
+            sendCommand(angles);
+        } else {
+            angles = (int) Math.round(360.0 / amtOfAngles) * 100
+                    + Command.AUTOMATIC_TURN_ANGLE;
+            if (amtOfAngles == 1)
+                sendCommand(length);
+            else
+                for (int i = 0; i < amtOfAngles; i++) {
+                    sendCommand(length);
+                    sendCommand(angles);
+                }
+        }
+    }
+
+    public int getSpeed() {
+        return simulationPilot.getSpeed();
+    }
+
+    public void setSpeed(int speed) {
+        if (speed == 1)
+            sendCommand(Command.SLOW_SPEED);
+        else if (speed == 2)
+            sendCommand(Command.NORMAL_SPEED);
+        else if (speed == 3)
+            sendCommand(Command.FAST_SPEED);
+        else
+            sendCommand(Command.VERY_FAST_SPEED);
+    }
+
+    public String getConsoleTag() {
+        if (robotConnected)
+            return "[ROBOT]";
+        return "[SIMULATOR]";
+    }
+
+    public void clear() {
+        simulationPilot.clear();
+    }
+
+    // TODO: vanaf hier!
+    /**
+     * Gets the amount of angles the arrow should turn in one event to be at par
+     * with the robot.
+     */
+    public double getAngularSpeed() {
+        switch (getSpeed()) {
+        case 1:
+            return (double) 1.82;
+        case 2:
+            return (double) 2.74;
+        case 3:
+            return (double) 2.77;
+        case 4:
+            return (double) 1.82;
+        }
+        return (double) 2.74;
+    }
+
+    public void goToNextTile(Orientation orientation) throws IOException {
+        double currentAngle = getStatusInfoBuffer().getAngle();
+        int angleToRotate = (int) ((double) orientation.getRightAngle() - currentAngle);
+        angleToRotate = (int) ExtMath.getSmallestAngle(angleToRotate);
+        sendCommand(angleToRotate * 100 + Command.AUTOMATIC_TURN_ANGLE);
+        sendCommand(40 * 100 + Command.AUTOMATIC_MOVE_FORWARD);
+    }
 
 }
--- SilverSurferPC/src/gui/SensorGraph.java
+++ SilverSurferPC/src/gui/SensorGraph.java
@@ -1,10 +1,11 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.font.*;
 import java.awt.geom.*;
 
-import javax.jws.soap.SOAPBinding.Use;
 import javax.swing.*;
 
 import java.util.ArrayList;
@@ -14,13 +15,15 @@ public class SensorGraph extends JPanel {
     private List<Integer> US = new ArrayList<Integer>();
     private List<Integer> LS = new ArrayList<Integer>();
 
-    {
+    public SensorGraph() {
         for (int i = 0; i < 100; i++) {
-            US.add(i);
-            LS.add(i * i);
+            US.add(0);
+            LS.add(0);
         }
+        new Timer(100, repaintSensorGraph).start();
     }
 
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
@@ -29,16 +32,53 @@ public class SensorGraph extends JPanel {
 
         int w = getWidth();
         int h = getHeight();
-        int xInc = w / 99;
+
+        int wLS = w / 2;
+        int wUS = w;
+
+        int LSWhite = 54;
+        int LSBlack = 34;
+        int LSMax = 57;
+        int LSMin = 31;
+        int LSScale = LSMax - LSMin;
+        int USMax = 150;
+        int USWall = 28;
+
+        // LightSensor white and black boundary
+        g2.draw(new Line2D.Double(0, h - (LSWhite - LSMin) * h / LSScale, wLS,
+                h - (LSWhite - LSMin) * h / LSScale));
+        g2.draw(new Line2D.Double(0, h - (LSBlack - LSMin) * h / LSScale, wLS,
+                h - (LSBlack - LSMin) * h / LSScale));
+
+        // USensor maximum useful value
+        g2.setColor(Color.red);
+        g2.draw(new Line2D.Double(wLS, h - USMax, wUS, h - USMax));
+        // USensor approximate wall values
+        g2.setColor(Color.black);
+        for (int wall = USWall; wall < USMax; wall += 40) {
+            g2.draw(new Line2D.Double(wLS, h - wall, wUS, h - wall));
+        }
 
         for (int i = 0; i < US.size() - 1; i++) {
-            g2.setColor(Color.blue);
-            g2.draw(new Line2D.Double(i * xInc, h - US.get(i), (i + 1) * xInc,
-                    h - US.get(i + 1)));
-            g2.setColor(Color.red);
-            g2.draw(new Line2D.Double(i * xInc, h - LS.get(i), (i + 1) * xInc,
-                    h - LS.get(i + 1)));
+            g2.setColor(Color.orange);
+            g2.draw(new Line2D.Double(i * w / 200, h - (LS.get(i) - LSMin) * h
+                    / LSScale, (i + 1) * w / 200, h - (LS.get(i + 1) - LSMin)
+                    * h / LSScale));
+
+            if (US.get(i) <= 150 && US.get(i + 1) <= 150) {
+                g2.setColor(Color.blue);
+                g2.draw(new Line2D.Double(wLS + i * w / 200, h - US.get(i), wLS
+                        + (i + 1) * w / 200, h - US.get(i + 1)));
+            } else {
+                g2.setColor(Color.lightGray);
+                g2.draw(new Line2D.Double(wLS + i * w / 200, h - 155, wLS
+                        + (i + 1) * w / 200, h - 155));
+            }
         }
+
+        // Dividing line between sensors
+        g2.setColor(Color.gray);
+        g2.fill(new Rectangle(wLS - 3, 0, 6, h));
     }
 
     public void addSensorValues(int USValue, int LSValue) {
@@ -46,9 +86,15 @@ public class SensorGraph extends JPanel {
         US.remove(0);
         LS.add(LSValue);
         LS.remove(0);
-
-        repaint();
     }
+
+    ActionListener repaintSensorGraph = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            repaint();
+        }
+    };
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
