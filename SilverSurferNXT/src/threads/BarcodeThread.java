d803c8d80c1cbc481f82f85bb5108436ebee3b1e
MaximVanMechelen <maximvanmechelen@hotmail.com>
2013-02-21 12:33:56 +0100

    package and sensor change

---
+++ SilverSurferNXT/src/threads/BarcodeThread.java
@@ -0,0 +1,40 @@
+package threads;
+
+import lejos.nxt.*;
+
+//Continuously searches for a barcode (= lightvalue < 40), stops the motors when one is found.
+public class BarcodeThread extends Thread {
+
+	private LightSensor lightSensor;
+	private boolean quit = false;
+	private boolean found = false;
+
+	public BarcodeThread(String str, LightSensor lightSensor) {
+		super(str);
+		this.lightSensor = lightSensor;
+	}
+	
+	@Override
+	public void run() {
+		while(!quit) {
+			try {
+				Thread.sleep(50);
+			} catch(Exception e) {
+	            System.out.println("Error in BarcodeThread.run()!");
+			}
+			if(lightSensor.getLightValue() < 40) {
+				found = true;
+				Motor.A.stop(true);
+				Motor.B.stop();
+			}
+		}
+	}
+	
+	public void setQuit(boolean quit) {
+		this.quit = quit;
+	}
+	
+	public boolean getFound() {
+		return found;
+	}
+}
