8e3284a1b23d3a4f63d4a3d3bc86a4be03a80daa
MaximVanMechelen <maximvanmechelen@hotmail.com>
2013-03-26 17:34:30 +0100

    localhost veranderd

--- SilverSurferPC/src/mq/communicator/Config.java
+++ SilverSurferPC/src/mq/communicator/Config.java
@@ -7,7 +7,7 @@ public interface Config {
     public static final String USER_NAME = "guest";
     public static final String PASSWORD = "guest";
     public static final String VIRTUAL_HOST = "/";
-    public static final String HOST_NAME = "leuven.cs.kotnet.kuleuven.be";
+    public static final String HOST_NAME = "localhost";
     public static final int PORT = 5672;
 
     // the default exchange that will be used for races during the official
